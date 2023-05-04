6ab3bd2e0e548380e774b51fbb6891a8a4402e7e
antariess <velislava.n.georgieva@gmail.com>
2023-05-04 14:36:04 +0100

    reshuffle

--- section-1/challenges.js
+++ section-1/challenges.js
@@ -1,210 +1,100 @@
-const { check, runTest, skipTest } = require("../test-api/index.js");
+const { check, runTest, skipTest } = require('../test-api/index.js');
 
-// Challenge 0
+// Exercise 1
+runTest('checking multiTypeArray', function () {
+  const multiTypeArray = ['I am a string', 42, true, [1, 2, 3]];
 
-// This function should be called return the product of two passed numbers.
-
-// multiply() {}
-
-runTest("multiply() can multiply 2 numbers together", function () {
-  check(multiply(3, 5)).isEqualTo(15);
-  check(multiply(17, 19)).isEqualTo(323);
-  check(multiply(-180, 2)).isEqualTo(-360);
+  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[0]);
+  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[1]);
+  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[2]);
+  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[3]);
 });
 
-// Challenge 1
+// Exercise 2
+// once you have passed the test above, change skipTest below to runTest so you are able to run the next test
+skipTest('checking alphaSample', function () {
+  const alphaSample = ['a', 'b', 'c'];
+  alphaSample.push('d');
+  alphaSample.push('g');
 
-// roundDown() {}
+  check(FILL_ME_IN).isEqualTo(alphaSample);
 
-// This function should take a single argument and return its value rounded DOWN to the nearest integer.
+  const lastItem = alphaSample.pop();
 
-skipTest(
-  "roundDown() returns the result of rounding down to the nearest integer",
-  function () {
-    check(roundDown(100.1)).isEqualTo(100);
-    check(roundDown(25.5)).isEqualTo(25);
-    check(roundDown(121.999)).isEqualTo(121);
-  }
-);
-
-// Challenge 2
-
-// This function should take two arguments, m and n, and return m raised to the power of n.
-
-// raiseToPower()
-
-skipTest("raiseToPower() raises given number to the given power", function () {
-  check(raiseToPower(10, 3)).isEqualTo(1000);
-  check(raiseToPower(25, 2)).isEqualTo(625);
-  check(raiseToPower(10, 0)).isEqualTo(1);
+  check(FILL_ME_IN).isEqualTo(lastItem);
+  check(FILL_ME_IN).isEqualTo(alphaSample);
 });
 
-// Challenge 3
+// Exercise 3
+// once you have passed the test above, change skipTest below to runTest so you are able to run the next test
+skipTest('working with nested arrays', function () {
+  const rows = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i']
+  ];
 
-// This function should take a number as an argument, and return true if it is a multiple of 6, and false otherwise.
+  check(FILL_ME_IN).isEqualTo(rows[0]);
+  check(FILL_ME_IN).isEqualTo(rows[1]);
+  check(FILL_ME_IN).isEqualTo(rows[2]);
 
-// isMultipleOf6()
+  const firstRow = rows[0];
+  check(firstRow[0]).isEqualTo('a');
+  check(firstRow[FILL_ME_IN]).isEqualTo('b');
 
-skipTest(
-  "isMultipleOf6() should check if a number is divisible by 6",
-  function () {
-    check(isMultipleOf6(6)).isEqualTo(true);
-    check(isMultipleOf6(10)).isEqualTo(false);
-    check(isMultipleOf6(15)).isEqualTo(false);
-    check(isMultipleOf6(36)).isEqualTo(true);
-    check(isMultipleOf6(60)).isEqualTo(true);
-    check(isMultipleOf6(61)).isEqualTo(false);
-  }
-);
-
-// Challenge 4
-
-// This function should take a string as an argument and return the same string with the first letter capitalised.
-
-// capitaliseFirstLetter()
-
-skipTest(
-  "capitaliseFirstLetter() capitalises the first letter in a string",
-  function () {
-    check(capitaliseFirstLetter("bang")).isEqualTo("Bang");
-    check(capitaliseFirstLetter("apple")).isEqualTo("Apple");
-    check(capitaliseFirstLetter("coding")).isEqualTo("Coding");
-  }
-);
-
-// Challenge 5
-
-// This function should take a number as an argument representing a year, 
-// and return true if that year is in the 20th century and false otherwise.
-
-// isInThe20thCentury()
-
-skipTest(
-  "isInThe20thCentury() checks if a number is within 1901 to 2000 (inclusive)",
-  function () {
-    check(isInThe20thCentury(1962)).isEqualTo(true);
-    check(isInThe20thCentury(1901)).isEqualTo(true);
-    check(isInThe20thCentury(1900)).isEqualTo(false);
-    check(isInThe20thCentury(1913)).isEqualTo(true);
-    check(isInThe20thCentury(1876)).isEqualTo(false);
-    check(isInThe20thCentury(2001)).isEqualTo(false);
-    check(isInThe20thCentury(2000)).isEqualTo(true);
-  }
-);
-
-// Challenge 6
-
-// This function should take a string as an argument representing a file path and return true if it is an absolute path, and false otherwise.
-
-// HINT: all absolute file paths start with a /
-
-// isAbsolutePath()
-
-skipTest(
-  "isAbsolutePath() checks if a file path is absolute or relative",
-  function () {
-    check(isAbsolutePath("/Users/mitch")).isEqualTo(true);
-    check(
-      isAbsolutePath(
-        "/Users/mitch/northcoders/remote_course/remote_precourse_1"
-      )
-    ).isEqualTo(true);
-    check(isAbsolutePath("../composers")).isEqualTo(false);
-    check(isAbsolutePath("./applications/my-awesome-app.js")).isEqualTo(false);
-  }
-);
-
-// Challenge 7
-
-/*
-
-This function should take a string as an argument and return a string which describes the ASCII code of that character
-
-The returned string should be in the following format:
-
-"The ASCII code for <character> is <character-code>"
-*/
-
-// getCharCode()
-
-skipTest(
-  "getCharCode() will return a message stating the ASCII code of a passed char",
-  function () {
-    check(getCharCode("A")).isEqualTo("The ASCII code for A is 65");
-    check(getCharCode("b")).isEqualTo("The ASCII code for b is 98");
-    check(getCharCode("z")).isEqualTo("The ASCII code for z is 122");
-    check(getCharCode("k")).isEqualTo("The ASCII code for k is 107");
-    check(getCharCode("!")).isEqualTo("The ASCII code for ! is 33");
-    check(getCharCode("M")).isEqualTo("The ASCII code for M is 77");
-  }
-);
-
-// Challenge 8
-
-// This function should take a length and a character as arguments and return an array of the given length populated with the given character.
-
-// createArray()
-
-skipTest(
-  "createArray() creates an array of the specified length using a specified character",
-  function () {
-    check(createArray(3, "!")).isEqualTo(["!", "!", "!"]);
-    check(createArray(5, "a")).isEqualTo(["a", "a", "a", "a", "a"]);
-  }
-);
-
-// Challenge 9
-/*
-This function should take a number representing a battery level as a percentage
-If the battery level is less than or equal to 5%, then you should return a string stating:
-    "Warning - battery level low: <number-here>%, please charge your device"
-If the battery level is between 5 and 99% then it should return a string stating:
-    "Battery level: <number-here>%"
-If the battery level is 100% then it should return a string stating:
-    "Fully charged :)"
-*/
-
-// checkBatteryLevel()
-
-skipTest(
-  "checkBatteryLevel() should return a message with info about the battery level",
-  function () {
-    check(checkBatteryLevel(100)).isEqualTo("Fully charged :)");
-
-    check(checkBatteryLevel(99)).isEqualTo("Battery level: 99%");
-    check(checkBatteryLevel(80)).isEqualTo("Battery level: 80%");
-    check(checkBatteryLevel(30)).isEqualTo("Battery level: 30%");
-    check(checkBatteryLevel(10)).isEqualTo("Battery level: 10%");
-    check(checkBatteryLevel(6)).isEqualTo("Battery level: 6%");
-
-    check(checkBatteryLevel(5)).isEqualTo(
-      "Warning - battery level low: 5%, please charge your device"
-    );
-    check(checkBatteryLevel(4)).isEqualTo(
-      "Warning - battery level low: 4%, please charge your device"
-    );
-    check(checkBatteryLevel(3)).isEqualTo(
-      "Warning - battery level low: 3%, please charge your device"
-    );
-    check(checkBatteryLevel(1)).isEqualTo(
-      "Warning - battery level low: 1%, please charge your device"
-    );
-  }
-);
-
-// Challenge 10
-
-// This function should take an array as an argument and return an array containing all string elements from the input (retaining the order)
-
-// collectStrings()
-
-skipTest("collectStrings() can get all the strings from an array", function () {
-  check(collectStrings(["a", "b", "c"])).isEqualTo(["a", "b", "c"]);
-  check(collectStrings(["a", 10, "b", 1000, "c"])).isEqualTo(["a", "b", "c"]);
+  check(rows[1][FILL_ME_IN]).isEqualTo('e');
+  check(rows[2][FILL_ME_IN]).isEqualTo('g');
+  check(rows[0][FILL_ME_IN]).isEqualTo('c');
 });
 
-/******* Refactor Bonus Challenge *******
+// Exercise 4
+// once you have passed the test above, change skipTest below to runTest so you are able to run the next test
+skipTest('Task 6', function () {
+  const father = {
+    firstName: 'Michael',
+    lastName: 'Bluth',
+    age: 33
+  };
 
-1. Challenge 5 - complete this problem without resorting to if statements!
-2. Refactor every function into an ES6 arrow function () => {}
-*/
+  check(FILL_ME_IN).isEqualTo(father.firstName);
+  check(FILL_ME_IN).isEqualTo(father.lastName);
+  check(FILL_ME_IN).isEqualTo(father.firstname);
+  check(FILL_ME_IN).isEqualTo(father.age);
+});
+
+// Exercise 5
+// once you have passed the test above, change skipTest below to runTest so you are able to run the next test
+skipTest('Task 7', function () {
+  const brotherInLaw = {
+    name: 'Tobias',
+    lastname: 'Funke',
+    job: 'therapist'
+  };
+
+  check(FILL_ME_IN).isEqualTo(brotherInLaw.job);
+  delete brotherInLaw.job;
+  check(FILL_ME_IN).isEqualTo(brotherInLaw.job);
+});
+
+// Exercise 6
+// once you have passed the test above, change skipTest below to runTest so you are able to run the next test
+skipTest('Task 8', function () {
+  const bluthFamily = {
+    father: {
+      name: 'George'
+    },
+    mother: {
+      name: 'Lucille'
+    },
+    sons: [{ name: 'GOB' }, { name: 'Michael' }, { name: 'Buster' }],
+    daughters: [{ name: 'Lindsay' }]
+  };
+
+  check(FILL_ME_IN).isEqualTo(bluthFamily.father.name);
+  check(bluthFamily.mother.name).isEqualTo(FILL_ME_IN);
+  check(bluthFamily.daughters[FILL_ME_IN].FILL_ME_IN).isEqualTo('Lindsay');
+});
+
+// >>>>>>>>>>> DON'T ALTER ANYTHING BELOW THIS LINE <<<<<<<<<<<<<<<
+
+var FILL_ME_IN;
